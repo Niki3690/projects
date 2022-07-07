8e61fbc1d31f6144e7743340040f1c1b625b56d7
niki <atmiyaniki@gmail.ccom>
2022-07-07 19:05:57 +0530

    added canva

---
+++ java-script/first.js
@@ -0,0 +1,228 @@
+// console.log("Hello World")
+
+
+// let num = 4
+// console.log(num)
+
+// num = 5
+// console.log(num)
+
+
+// let username ="Niki"
+// console.log(username)
+
+
+// let radius = 5
+// let pi = 3.14
+
+
+// area = pi * radius * radius
+// console.log(area)
+
+
+// console.log(Number.MAX_VALUE * 10)
+
+// let firstName = "Niki"
+// let lastName= "Patel"
+
+// let user=firstName + lastName
+// console.log(user)
+
+// let firstName = "Niki"
+// let lastName= "Patel"
+
+// let user=firstName + " " + lastName
+// console.log(user)
+
+// let user = "Niki \n Patel"
+// console.log(user)
+
+// let bool = 5 < 6
+// console.log(bool)
+
+// bool= 5  > 6
+// console.log(bool)
+
+
+// let num = 6
+// console.log(num, typeof num)
+
+
+// let x
+// console.log(x, typeof x)
+// x = 8
+// console.log(x, typeof x)
+// x=+x + 2
+// console.log(x, typeof x)
+// x = x - 2
+// console.log(x, typeof x)
+// x = !x
+// console.log(x, typeof x)
+// x = +x + 2
+// console.log(x, typeof x)
+
+// let x ="369 Niki"
+// console.log(x)
+
+// x= Number("369 Niki")
+// console.log(x)
+
+// x= parseInt("369 Niki")
+// console.log(x)
+
+
+
+// let num1=5
+// let num2=6
+
+// let result = num1 + num2
+// console.log(result)
+// result = num1 - num2
+// console.log(result)
+// result = num1 * num2
+// console.log(result)
+// result = num1 / num2
+// console.log(result)
+// result = num1 % num2
+// console.log(result)
+
+
+// let num=4
+// // num = num -1   
+// num--
+// console.log(num)
+
+
+// let num=4
+// // num=num  + 1
+// num++
+// console.log(num)
+
+
+// let num=4
+// let result = num* num* num
+// console.log(result)
+
+
+// let num=4
+// let result=Math.pow(4,3)
+// console.log(result)
+
+
+// let num=4
+// let result= 4 ** 3
+// console.log(result)
+
+
+// let x=7 , y=8 ,z=9
+// let result=x < y &&  x < z
+// console.log(result)
+
+
+
+// let num1=6;
+// let  num2=4;
+// let result=num1>num2;
+
+// if(result)
+// console.log("num1 is greater");
+// console.log("Hiii....")
+
+
+// let num1=3;
+// let  num2=4;
+// let result=num1>num2;
+
+// if(result)
+// console.log("num1 is greater");
+// console.log("Hiii....")
+
+// let num1=10;
+// let num2=9;
+// let  num3=7;
+
+// if(num1>num2 && num1>num3){
+// console.log("num1 is greater");
+// }
+// else if(num2 > num3){
+// console.log("num2 is greater")
+// }
+// else{
+// console.log("num3 is greater")    
+// console.log("Hiii....")
+// }
+
+
+
+
+// let num=8
+// let result
+// result =num%2===0  ? "Even" : "Odd"
+// console.log(result)
+
+// let num=5
+// let result
+// result =num%2===0  ? "Even" : "Odd"
+// console.log(result)
+
+
+//monday-7am
+//tuesday-thursday -4am
+//friday -9am
+//saturday-sunday -8am
+
+// let day="holiday"
+
+// switch(day){
+//     case "Monday":
+//         console.log("7am")
+//         break;
+//     case "Tuesday":
+//         console.log("4am")
+//         break;
+//     case "Wednesday":
+//         console.log("4am")
+//         break;
+//     case "Thursday":
+//         console.log("4am")
+//         break;
+//     case "Friday":
+//         console.log("9am")
+//         break;
+//     case "Saturday":
+//         console.log("8am")
+//         break;
+//     case "Sunday":
+//         console.log("8am")
+//         break;
+//     default:
+//         console.log("Sleepy")
+// }
+// console.log("Wakeup..")
+
+
+
+// let num1=9
+// let num2=4
+// let result=num1 + num2
+
+// console.log(`The addition of ${num1} and ${num2} is ${result}`);
+
+
+// let i=1;
+// while(i<=2)
+// {
+//     console.log("Hiii");
+//     i++
+// }
+
+
+for(let i=1;i<=6;i++)
+{
+    console.log("bby",i);
+}
+
+
+
+
+
